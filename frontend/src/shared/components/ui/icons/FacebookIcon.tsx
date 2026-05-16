2c976ccd5221180439cb7c6db8d68ff7164f5bce
Stanislav Trubitsyn <trubitsynstanislav2004@gmail.com>
2026-05-16 23:06:50 +0300

    feat(frontend): add footer translations and custom social SVG icons

---
+++ frontend/src/shared/components/ui/icons/FacebookIcon.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import { SvgIcon, SvgIconProps } from '@mui/material'
+
+export const FacebookIcon = (props: SvgIconProps) => (
+	<SvgIcon {...props} viewBox='0 0 12 20'>
+		<g clipPath='url(#clip0_228_29443)'>
+			<path
+				d='M11.0269 0.183259L8.07507 0.179688C5.21257 0.179688 3.36346 2.07612 3.36346 5.01451V7.24219H0.401855V11.2734H3.36346L3.35989 19.8225H7.50364L7.50721 11.2734H10.9054L10.9027 7.24308H7.50721V5.3529C7.50721 4.44397 7.72239 3.98415 8.90543 3.98415L11.0179 3.98326L11.0269 0.183259Z'
+				fill='currentColor'
+			/>
+		</g>
+		<defs>
+			<clipPath id='clip0_228_29443'>
+				<rect width='11.4286' height='20' fill='white' />
+			</clipPath>
+		</defs>
+	</SvgIcon>
+)
